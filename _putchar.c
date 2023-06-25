a69250d251abf238cf45327cd9b355826091c113
root <adeutiadedayo@gmail.com>
2023-06-25 13:49:05 -0700

    defined conversion and putchar

--- _putchar.c
+++ _putchar.c
@@ -1,11 +1,11 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes a character to standard output
+ * write_ch - writes a character to standard output
  * @a: character to print
  * Return: 1 on success, -1 is on error appropriately.
  */
-int _putchar(char a)
+int write_ch(char a)
 {
 	return (write(1, &a, 1));
 }
