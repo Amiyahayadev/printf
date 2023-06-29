cc599780ca3a065f6f84d30c5ad2f99b56d817bb
Ms Amina Yahaya Baba <ameenahyahayab@gmail.com>
2023-06-29 22:03:56 +0100

    Update and rename conversion of % p_x_X

--- conversion-p_x_X.c
+++ conversion-p_x_X.c
@@ -1,34 +1,5 @@
 #include "main.h"
 
-/* Forward declaration */
-int write_ch(char a);
-
-
-/**
- * print_decimal - prints argument as decimal
- * @ap: other variadic arguments list
- * Return: number of characters printed
- */
-int print_decimal(va_list ap)
-{
-	int num, len = 0;
-
-	num = va_arg(ap, int);
-
-	if (num == 0)
-	{
-		len += write_ch('0');
-		return (len);
-	}
-	if (num < 0)
-	{
-		len += write_ch('-');
-		num = -num;
-	}
-	len += write_unsigned_decimal((unsigned int)num);
-	return (len);
-}
-
 /**
  * print_pointer - prints argument as pointer addre
  * @ap: other variadic arguments list
@@ -49,16 +20,6 @@ int print_pointer(va_list ap)
 }
 
 /**
- * write_ch - writes a character to standard output
- * @a: character to print
- * Return: 1 on success, -1 on error
- */
-int write_ch(char a)
-{
-	return (write(1, &a, 1));
-}
-
-/**
  * write_hexadecimal - writes an unsigned hexadecimal value to standard output
  * @num: unsigned hexadecimal value to print
  * Return: number of characters printed
@@ -68,19 +29,52 @@ int write_hexadecimal(unsigned long num)
 	int len = 0;
 
 	if (num >= 16)
-	{
 		len += write_hexadecimal(num / 16);
-	}
+
 	int remainder = num % 16;
 
 	if (remainder < 10)
-	{
 		len += write_ch('0' + remainder);
-	}
 	else
-	{
 		len += write_ch('a' + (remainder - 10));
-	}
+
 	return (len);
 }
 
+int write_hexadecimal(unsigned int num, int uppercase)
+{
+    int remainder;
+    int len = 0;
+
+    if (num == 0)
+        return write_ch('0');
+
+    if (num >= 16)
+        len += write_hexadecimal(num / 16, uppercase);
+
+    remainder = num % 16;
+
+    if (remainder < 10)
+        len += write_ch('0' + remainder);
+    else
+    {
+        if (uppercase)
+            len += write_ch('A' + (remainder - 10));
+        else
+            len += write_ch('a' + (remainder - 10));
+    }
+
+    return len;
+}
+
+int print_hexadecimal(va_list ap)
+{
+    unsigned int num = va_arg(ap, unsigned int);
+    return write_hexadecimal(num, 0);
+}
+
+int print_hexa_upper(va_list ap)
+{
+    unsigned int num = va_arg(ap, unsigned int);
+    return write_hexadecimal(num, 1);
+}
